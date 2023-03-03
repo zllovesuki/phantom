de31db9db1d0cf7a5ed122e4faacc952257a4710
Rachel Chen <rachel@chens.email>
2023-03-03 02:54:43 -0800

    increase max-width to screen-2xl

--- frontend/src/components/NavBar.vue
+++ frontend/src/components/NavBar.vue
@@ -44,7 +44,7 @@ const navigation = routes
     <div
       :class="[
         PaddingTop ? 'pt-4' : '',
-        'mx-auto max-w-screen-xl px-4 sm:px-6 lg:px-8',
+        'mx-auto max-w-screen-2xl px-4 sm:px-6 lg:px-8',
       ]"
     >
       <div class="flex h-16 items-center justify-between">
